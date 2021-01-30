429c1db649fe77de74e5d5a30cc60bc0a14468d1
xjl <18253040259@163.com>
2021-01-31 01:04:24 +0800

    修改

--- Week09/server/server.go
+++ Week09/server/server.go
@@ -27,28 +27,35 @@ func main() {
 // 处理一个链接
 func handleConn(conn net.Conn) {
 	msg := make(chan string, 1)
+	exit := make(chan string)
 	defer conn.Close()
 	defer close(msg)
+	defer close(exit)
 	go send(conn, msg)
-	go receive(conn, msg)
-	select {}
+	go receive(conn, msg, exit)
+	select {
+	case <-exit:
+		fmt.Printf("%s退出\n",conn.RemoteAddr().String())
+		return
+	}
 }
 
-func send(conn net.Conn, message <-chan string) {
-	for msg := range message {
-		fmt.Println(conn, msg)
+func send(conn net.Conn, message chan string) {
+	for {
+		msg := <-message
 		_, err := conn.Write([]byte("reviewed" + msg))
 		if err != nil {
-			fmt.Println("write err %+v", err)
+			fmt.Printf("write err %+v", err)
 			return
 		}
 	}
 }
 
-func receive(conn net.Conn, message chan string) {
+func receive(conn net.Conn, message chan string, exit chan string) {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		fmt.Printf("message: %s\n", scanner.Text())
 		message <- scanner.Text()
 	}
+	exit <- "q"
 }
